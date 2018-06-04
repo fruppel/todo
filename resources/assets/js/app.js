d87576a0c9844b77a11d584390e8444c6f95f508
fruppel <skywalker9947>
2018-06-04 20:33:35 +0200

    Add some content back

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,9 +1,9 @@
 // require('./bootstrap');
 
-window.Vue = require('vue');
-
-Vue.component('tasks', require('./components/Tasks'));
-
-const app = new Vue({
-    el: '#app'
-});
+// window.Vue = require('vue');
+//
+// Vue.component('tasks', require('./components/Tasks'));
+//
+// const app = new Vue({
+//     el: '#app'
+// });
