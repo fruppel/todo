19756e8b65ed20c396bf9931f1c60485eb3666bb
fruppel <skywalker9947>
2018-06-04 20:31:34 +0200

    Add some content back

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,14 @@
 <html lang="{{ app()->getLocale() }}">
 <head>
     <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <link rel="manifest" href="{{ asset('manifest.json') }}">
+
+    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
