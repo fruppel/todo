2c055ef3c5ffc3fa379d3d615a17a79c663119ad
fruppel <skywalker9947>
2018-06-05 21:24:59 +0200

    Fix date input

--- resources/views/tasks/_form.blade.php
+++ resources/views/tasks/_form.blade.php
@@ -1,13 +1,13 @@
 {{ csrf_field() }}
 
 <div class="form-group">
-    {{ Form::label('description', 'Beschreibung') }}
+    <label for="description">Beschreibung</label>
     {{ Form::text('description', null, ['class' => 'form-control']) }}
 </div>
 
 <div class="form-group">
-    {{ Form::label('day', 'Datum') }}
-    {{ Form::date('day', $task->day ?? \Carbon\Carbon::now()->format('Y-m-d'), ['class' => 'form-control']) }}
+    <label for="day">Datum</label>
+    <input type="date" id="day" name="day" :value.prop="'{{ old('day', $task->day ?? \Carbon\Carbon::now()->format('Y-m-d')) }}'" class="form-control">
 </div>
 
 <div class="form-group mt-5">
